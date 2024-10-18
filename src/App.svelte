c909752533b5cb402f7b0f6d6b2a0b34f2abd3cd
Matteo Piazza <matteo.piazza@gmail.com>
2024-10-18 13:41:21 +0200

    more data

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,12 @@
   import ResourceSchedulerFlipped from './lib/ResourceSchedulerFlipped.svelte';
 
   const resources = [
-    { id: 1, name: 'Worker 1' },
-    { id: 2, name: 'Worker 2' },
-    { id: 3, name: 'Worker 3' },
-    { id: 4, name: 'Worker 4' },
+    { id: 1, name: 'Tecnico 1' },
+    { id: 2, name: 'Tecnico 2' },
+    { id: 3, name: 'Tecnico 3' },
+    { id: 4, name: 'Tecnico 4' },
+    { id: 5, name: 'Tecnico 5' },
+    { id: 6, name: 'Tecnico 6' }
   ];
 
   const events = [
@@ -14,6 +16,8 @@
     { id: 2, resourceId: 2, title: 'Task 2 one one one', start: '2024-10-17T13:00:00', end: '2024-10-17T14:00:00' },
     { id: 3, resourceId: 3, title: 'Task 3', start: '2024-10-17T11:00:00', end: '2024-10-17T15:00:00' },
     { id: 4, resourceId: 4, title: 'Task 4', start: '2024-10-17T12:00:00', end: '2024-10-17T13:00:00' },
+    { id: 5, resourceId: 5, title: 'Task 5', start: '2024-10-18T12:00:00', end: '2024-10-18T13:00:00' },
+    { id: 6, resourceId: 6, title: 'Task 6', start: '2024-10-18T08:00:00', end: '2024-10-18T13:00:00' }
   ];
 </script>
 
